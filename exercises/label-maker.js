0420b56eebb88fefad18ceaee27641feaff80cfd
Eshei Jenkins <eshjenk@gmail.com>
2024-10-21 15:04:01 -0400

    label mader & product code practice

---
+++ exercises/label-maker.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let contact = {
+  name: "Eshei Brown",
+  address: "5 Broadway Street",
+  city: "Queens",
+  state: "New York",
+  zip: 11234,
+};
+
+function printLabel(contact) {
+  console.log(contact);
+}
+printLabel(contact);
+
+let myInfo = {
+  name: "Eshei Brown",
+  address: "5 Broadway Street",
+  city: "Queens",
+  state: "New York",
+  zip: 11234,
+};
+
+function printContact(myInfo) {
+  console.log(contact.name);
+  console.log(contact.address);
+  console.log(contact.city + ",", contact.state, +" " + contact.zip);
+}
+function printLabel() {
+let mailingLabel = '
+${contact.name}
+${contact.address}
+${contact.city}, ${contact.state} ${contact.zip}';
+
+console.log(mailingLabel);
+}
+
+printLabel();
+
---
+++ exercises/product-code.js
@@ -0,0 +1,10 @@
+"use strict";
+const code1 = "ACME:123-L";
+function parsePartCode(code) {
+  let code1 = {
+    supplierCode: "ACME",
+    productNumber: "123",
+    size: "L",
+  };
+}
+parsePartCode(code1);
