9addd5e6ea22d3a5f329a7352731d6fe14530e52
Eshei Jenkins <eshjenk@gmail.com>
2024-10-21 15:02:34 -0400

    loops exercises

--- demos/for-loop-count-evens.js
+++ demos/for-loop-count-evens.js
@@ -1,5 +1,5 @@
 "use strict";
-let number = 1;
+
 
 //before loop;condition to keep looping; at the end of each loop
 for(let count=0;count<=100; count = count +1){
---
+++ exercises/LoopScripts/for-loop2.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// EXERCISE 2
+// let count = 1;
+// while (count <= 7) {
+//   console.log("I love loops");
+//   count = count + 1
+// }
+
+
+
+
+//EXERCISE 1
+
+for (let count = 1; count <= 7; count = count + 1) {
+    console.log("I love loops! " + count);
+}
+
+
