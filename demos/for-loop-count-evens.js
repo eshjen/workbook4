eee81a7c3aebf7635f20c307c8d7d749f5dee8e0
Eshei Jenkins <eshjenk@gmail.com>
2024-10-21 14:47:41 -0400

    while loops exercise 2

--- demos/for-loop-count-evens.js
+++ demos/for-loop-count-evens.js
@@ -2,7 +2,7 @@
 let number = 1;
 
 //before loop;condition to keep looping; at the end of each loop
-for(let count=0;count<100; count = count +1){
+for(let count=0;count<=100; count = count +1){
     number = number * 2;
     console.log(number);
     
