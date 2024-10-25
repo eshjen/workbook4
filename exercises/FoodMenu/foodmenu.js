49b54f433f5d692539e501af12ebe792b9f3703a
Eshei Jenkins <eshjenk@gmail.com>
2024-10-25 16:04:02 -0400

    food menu complete

--- exercises/FoodMenu/foodmenu.js
+++ exercises/FoodMenu/foodmenu.js
@@ -1,18 +1,33 @@
 "use strict";
 let menu = {
-    drinks : [
-     "Water", "Tea", "Sweet Tea",
-     "Coke", "Dr. Pepper", "Sprite"
-     ],
-    entrees : [
+  drinks: ["Water", "Tea", "Sweet Tea", "Coke", "Dr. Pepper", "Sprite"],
+  entrees: [
     "Hamburger w/ Fries",
-     "Grilled Cheese w/ Tater Tots",
+    "Grilled Cheese w/ Tater Tots",
     "Grilled Chicken w/ Veggies",
-     "Chicken Fried Steak w/ Mashed Potatoes",
+    "Chicken Fried Steak w/ Mashed Potatoes",
     "Fried Shrimp w/ Coleslaw",
-     "Veggie Plate"
-    ],
-    desserts: [
-     "Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
-     ]
-    };
+    "Veggie Plate",
+  ],
+  desserts: ["Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"],
+};
+
+let categoryItems = document.getElementById("categoryItems");
+let categoryItems2 = document.getElementById("categoryItems2");
+
+function showDetails() {
+    categoryItems2.innerHTML = ""
+  for (const item in menu) {
+    console.log(item);
+    console.log(categoryItems.value);
+    
+    if (categoryItems.value == item) {
+      for (const detailedItem of menu[item]) {
+        console.log(detailedItem);
+        let option = document.createElement("option");
+        option = new Option(detailedItem, menu[item].indexOf(detailedItem));
+        categoryItems2.append(option);
+      }
+    }
+  }
+}
